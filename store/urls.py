a124e626c50a59aa2d8ad26f41a114deb73098f2
andre <andd68@hotmail.com>
2021-06-09 11:29:54 -0300

    add search function

--- store/urls.py
+++ store/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
 from .views import store
 from .views import product_detail
+from .views import search
 
 urlpatterns = [
     path('', store, name='store'),
-    path('<slug:category_slug>/', store, name='products_by_category'),
-    path('<slug:category_slug>/<slug:product_slug>/', product_detail, name='product_detail'),
+    path('category/<slug:category_slug>/', store, name='products_by_category'),
+    path('category/<slug:category_slug>/<slug:product_slug>/', product_detail, name='product_detail'),
+    path('search/', search, name='search'),
 ]
--- store/views.py
+++ store/views.py
@@ -1,3 +1,4 @@
+from django.db.models import Q
 from django.shortcuts import render, get_object_or_404
 from .models import Product
 from category.models import Category
@@ -37,3 +38,19 @@ def product_detail(request, category_slug, product_slug):
         raise e
     context = {'single_product': single_product, 'in_cart': in_cart}
     return render(request, 'store/product_detail.html', context)
+
+
+def search(request):
+    products = {}
+    product_count = 0
+    if 'keyword' in request.GET:
+        keyword = request.GET['keyword']
+        if keyword:
+            products = Product.objects.order_by('-created_date').filter(Q(description__icontains=keyword)
+                                                                        | Q(product_name__icontains=keyword))
+            product_count = products.count()
+    context = {
+        'products': products,
+        'product_count': product_count,
+    }
+    return render(request, 'store/store.html', context)
