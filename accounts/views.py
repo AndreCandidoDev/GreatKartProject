2e526880d2e11f51a10dc023828bb1c6100ed3ce
andre <andd68@hotmail.com>
2021-06-10 15:13:03 -0300

    add login and logout functions

--- accounts/views.py
+++ accounts/views.py
@@ -1,4 +1,5 @@
-from django.contrib import messages
+from django.contrib import messages, auth
+from django.contrib.auth.decorators import login_required
 from django.http import HttpResponse
 from django.shortcuts import render, redirect
 from .forms import RegistrationForm
@@ -16,7 +17,7 @@ def register(request):
             password = form.cleaned_data['password']
             username = email.split('@')[0]
             user = Account.objects.create_user(first_name=first_name, last_name=last_name, email=email,
-                                          username=username, password=password)
+                                               username=username, password=password)
             user.phone_number = phone_number
             user.save()
             messages.success(request, 'Registration successful!')
@@ -30,8 +31,23 @@ def register(request):
 
 
 def login(request):
+    if request.method == 'POST':
+        email = request.POST['email']
+        password = request.POST['password']
+        user = auth.authenticate(email=email, password=password)
+        if user is not None:
+            auth.login(request, user)
+            # messages.success(request, 'You are now logged in!')
+            return redirect('home')
+        else:
+            messages.error(request, 'Invalid login credentials')
+            return redirect('login')
     return render(request, 'accounts/login.html')
 
 
+@login_required(login_url='login')
 def logout(request):
-    return
+    auth.logout(request)
+    messages.success(request, 'You are logged out.')
+    return redirect('login')
+
