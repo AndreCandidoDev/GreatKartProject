ca69f388b5fc564062462f9f36c3417d2db3ba3c
andre <andd68@hotmail.com>
2021-06-14 13:57:46 -0300

    add checkout and cart bug fixes

--- accounts/views.py
+++ accounts/views.py
@@ -11,6 +11,9 @@ from django.utils.http import urlsafe_base64_encode, urlsafe_base64_decode
 
 from .forms import RegistrationForm
 from .models import Account
+from carts.models import Cart, CartItem
+from carts.views import _cart_id
+import requests
 
 
 def register(request):
@@ -59,9 +62,55 @@ def login(request):
         password = request.POST['password']
         user = auth.authenticate(email=email, password=password)
         if user is not None:
+            try:
+                cart = Cart.objects.get(cart_id=_cart_id(request))
+                is_cart_item_exists = CartItem.objects.filter(cart=cart).exists()
+                if is_cart_item_exists:
+                    cart_item = CartItem.objects.filter(cart=cart)
+
+                    # Getting the product variations by cart id
+                    product_variation = []
+                    for item in cart_item:
+                        variation = item.variations.all()
+                        product_variation.append(list(variation))
+
+                    # Get the cart items from the user to access his product variations
+                    cart_item = CartItem.objects.filter(user=user)
+                    ex_var_list = []
+                    id = []
+                    for item in cart_item:
+                        existing_variation = item.variations.all()
+                        ex_var_list.append(list(existing_variation))
+                        id.append(item.id)
+                    for pr in product_variation:
+                        if pr in ex_var_list:
+                            index = ex_var_list.index(pr)
+                            item_id = id[index]
+                            item = CartItem.objects.get(id=item_id)
+                            item.quantity += 1
+                            item.user = user
+                            item.save()
+                        else:
+                            cart_item = CartItem.objects.filter(cart=cart)
+                            for item in cart_item:
+                                item.user = user
+                                item.save()
+            except:
+                pass
             auth.login(request, user)
             messages.success(request, 'You are now logged in!')
-            return redirect('dashboard')
+            url = request.META.get('HTTP_REFERER')
+            try:
+                query = requests.utils.urlparse(url).query
+                # query = next=/cart/checkout/
+                params = dict(x.split('=') for x in query.split('&'))
+                # params = {'next': '/cart/checkout/'}
+                if 'next' in params:
+                    nextpage = params['next']
+                    return redirect(nextpage)
+            except:
+                return redirect('dashboard')
+            # return redirect('dashboard')
         else:
             messages.error(request, 'Invalid login credentials')
             return redirect('login')
--- carts/urls.py
+++ carts/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import cart, add_cart, remove_cart, remove_cart_item
+from .views import cart, add_cart, remove_cart, remove_cart_item, checkout
 
 urlpatterns = [
     path('', cart, name='cart'),
     path('add_cart/<int:product_id>/', add_cart, name='add_cart'),
     path('remove_cart/<int:product_id>/<int:cart_item_id>/', remove_cart, name='remove_cart'),
     path('remove_cart_item/<int:product_id>/<int:cart_item_id>/', remove_cart_item, name='remove_cart_item'),
+    path('checkout/', checkout, name='checkout'),
 ]
