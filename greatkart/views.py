d3f1928138966a0be85a3c5ee6dd84b595bef864
andre <andd68@hotmail.com>
2021-06-20 13:06:03 -0300

    fix home view

--- greatkart/views.py
+++ greatkart/views.py
@@ -3,6 +3,7 @@ from store.models import Product, ReviewRating
 
 
 def home(request):
+    reviews = None
     products = Product.objects.all().filter(is_available=True).order_by('created_date')
     for product in products:
         reviews = ReviewRating.objects.filter(product_id=product.id, status=True)
